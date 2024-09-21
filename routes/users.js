ae346e90f6dd1bdea3039902e150afc7620234de
Franco Cerda <frankoadvance20@gmail.com>
2024-09-20 21:09:53 -0300

    update project
    creacion de get, post y put, para los usuarios

--- routes/users.js
+++ routes/users.js
@@ -36,4 +36,95 @@ router.post('/register', async (req, res) => {
 });
 
 
+//Obtenemos todos los usuarios con sus parametros
+router.get('/', async (req,res) =>{
+
+  try {
+    const result = await pool.query('SELECT id, name, email FROM users');
+    res.json(result.rows);
+  } catch(error){
+    console.error('Error al obtener los usuarios:', error.message); // Mostrar el error exacto en la consola
+    res.status(500).json({error: 'Error al obtener los usuarios'}); // Mostrar el error exacto en la respuesta del json, postman
+  }
+});
+
+
+//Actualizar un usuario
+router.put('/:id', async (req, res) => {
+  
+    const {name , email} = req.body;
+    const {id} = req.params;
+  
+  try{
+    const result = await pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name,email,id] // mando consulta a postgres, y despues los placeholders se reemplazan por los param que les paso
+    );
+
+    if (result.row.length == 0){
+      return res.status(404).json({error : 'Usuario no econtrado'}); // si lo buscamos y no esta devolvemos altiro 
+    }
+    res.json(result.rows[0]); // devolvemos el primero que obtuvimos
+  } catch(error){
+    console.error('Error al actualizar el usuario', error);// Consola
+    res.status(500).json({error : 'Error el actualizar usuario'}) //Postman
+  }
+});
+
+
+router.delete('/:id', async (req,res) => {
+
+  const {id} = req.params;
+
+  try{
+
+    const result = await pool.query(
+      'DELETE FROM users WHERE id = $1 RETURNING *', [id]
+    );
+    if (result.row.lenght == 0){
+      return res.status(404).json({error : 'Usuario no encontrado'})''
+    }
+    res.json({message: 'Usuario eliminado con exito'});
+    
+  } catch (error){
+    console.error('Error al eliminar el usuario', error.message);
+    res.status(500).json({error : 'Error al eliminar el usuario'});
+
+  }
+
+
+
+
+});
+
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: 'Usuario no encontrado' });
+    }
+
+    const user = result.rows[0];
+    // Comparar las contraseñas
+    const isMatch = await bcrypt.compare(password, user.password); // booleano que revisa si son iguales
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Contraseña incorrecta' });
+    }
+
+    // aqui se puede implementar un token, le damos al usuario privilegios, ejemplo: guardar productos en el carro
+    res.json({ message: 'Inicio de sesión exitoso' });
+
+  } catch (err) {
+    console.error('Error al iniciar sesión:', err.message);
+    res.status(500).json({ error: 'Error al iniciar sesión' });
+  }
+}); 
+
+
+
+
+
+
 module.exports = router;
